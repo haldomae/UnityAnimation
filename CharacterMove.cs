56172638c68ebc1f2238adcd515d4323a66a80f7
haldomae <hal.domae@gmail.com>
2026-01-28 15:32:14 +0900

    コードを修正し、斜めの移動を滑らかに

--- CharacterMove.cs
+++ CharacterMove.cs
@@ -15,34 +15,17 @@ public class CharactorMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Wキーが押された間、前方に移動
-        if(Input.GetKey(KeyCode.W)){
-            // transformのz軸を更新
-            // GetComponent<Transform>()は省略可能
-            // Vector3.forward = new Vector3(0,0,1)
-            // Time.deltaTimeフレームレートの影響受けなくなる
-            transform.position += 
-            Vector3.forward * moveSpeed * Time.deltaTime;
-        }
-        // Sキーが押された間、後方に移動
-        if(Input.GetKey(KeyCode.S)){
-            // Vector3.back = new Vector3(0,0,-1)
-            transform.position += 
-            Vector3.back * moveSpeed * Time.deltaTime;
-        }
 
-        // Aキーが押された間、左方向に移動
-        if(Input.GetKey(KeyCode.A)){
-            // Vector3.left = new Vector3(-1,0,0)
-            transform.position += 
-            Vector3.left * moveSpeed * Time.deltaTime;
-        }
+        // キー入力値
+        float horizontal = Input.GetAxis("Horizontal"); // A,D
+        float vertical = Input.GetAxis("Vertical"); // W,S
 
-        // Dキーが押された間、右方向に移動
-        if(Input.GetKey(KeyCode.D)){
-            // Vector3.right = new Vector3(1,0,0)
-            transform.position += 
-            Vector3.right * moveSpeed * Time.deltaTime;
-        }
+        // 移動方向を計算
+        Vector3 moveDirection = 
+        new Vector3(horizontal, 0 , vertical);
+
+        // 座標移動
+        transform.position += 
+        moveDirection * moveSpeed * Time.deltaTime;
     }
 }
